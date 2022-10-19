1de6659a37d98c4bf289dea0503c06c983e66b1a
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-10-19 13:02:36 -0300

    ajustando blade

--- projeto_portfolio/resources/views/contact.blade.php
+++ projeto_portfolio/resources/views/contact.blade.php
@@ -15,32 +15,32 @@
                             <div class="head">Nome</div>
                             <div class="sub-title">Juliane Bezerra</div>
                         </div>
-                   </div>
-                   <div class="row">
-                    <ion-icon name="earth-outline"></ion-icon>
-                    <div class="info">
-                        <div class="head">Endereço</div>
-                        <div class="sub-title">Crato, Ce</div>
                     </div>
-               </div>
-               <div class="row">
-                <ion-icon name="person-outline"></ion-icon>
-                <div class="info">
-                    <div class="head">Email</div>
-                    <div class="sub-title">fbjuliane@gmail.com</div>
+                    <div class="row">
+                     <ion-icon name="earth-outline"></ion-icon>
+                     <div class="info">
+                         <div class="head">Endereço</div>
+                         <div class="sub-title">Crato, Ce</div>
+                     </div>
                 </div>
-            </div>
-        </div>
-            </div> 
-            {{-- column left --}}
-            <div class="column rigth">
-                <div class="text">Mensagem</div>
-                <form action="#">
-                    <div class="fields">
-                        <div class="field name">
-                            <input type="text" placeholder="Nome" required>
-                        </div>
-                        <div class="field email">
+                <div class="row">
+                 <ion-icon name="person-outline"></ion-icon>
+                 <div class="info">
+                     <div class="head">Email</div>
+                     <div class="sub-title">fbjuliane@gmail.com</div>
+                 </div>
+             </div>
+         </div>
+             </div> 
+             {{-- column left --}}
+             <div class="column rigth">
+                 <div class="text">Mensagem</div>
+                 <form action="#">
+                     <div class="fields">
+                         <div class="field name">
+                             <input type="text" placeholder="Nome" required>
+                         </div>
+                         <div class="field email">
                             <input type="email" placeholder="Email" required>
                         </div>
                     </div>
@@ -49,20 +49,20 @@
                             <input type="text" placeholder="Sobrenome" required>
                         </div>
                         <div class="field textarea">
-                           <textarea  cols="30" rows="10" placeholder="Escrever..." required></textarea>
-                        </div>
-                        <div class="button">
-                            <button type="submit">Enviar</button> </div> <br> 
-                            <a class="insta" href="https://www.instagram.com/juliane_bzrr/" target="_blank"><img src="/css/images/insta.png" width="50px" alt=""></a>
-                            <a class="github" href="https://github.com/Julianebf" target="_blank"><img src="/css/images/github.png" width="50px" alt=""></a> 
-                            <a class="linkdin" href="https://www.linkedin.com/in/juliane-bezerra-ferreira/" target="_blank"><img src="/css/images/linkedin.png" width="50px" alt=""></a>
-                            <a class="whats" href="https://wa.me/5588997801433" target="_blank"><img src="/css/images/whatss.png" width="50px" alt=""></a>  
-                        </div>
-                    </div>
-                </form>
-            </div>
+                            <textarea  cols="30" rows="10" placeholder="Escrever..." required></textarea>
+                         </div>
+                         <div class="button">
+                             <button type="submit">Enviar</button> </div> <br> 
+                             <a class="insta" href="https://www.instagram.com/juliane_bzrr/" target="_blank"><img src="{{ asset ('/css/images/insta.png')}}" width="50px" alt=""></a>
+                             <a class="github" href="https://github.com/Julianebf" target="_blank"><img src="{{ asset('/css/images/github.png')}}" width="50px" alt=""></a> 
+                             <a class="linkdin" href="https://www.linkedin.com/in/juliane-bezerra-ferreira/" target="_blank"><img src="{{ asset('/css/images/linkedin.png')}}" width="50px" alt=""></a>
+                             <a class="whats" href="https://wa.me/5588997801433" target="_blank"><img src="{{ asset('/css/images/whatss.png')}}" width="50px" alt=""></a>  
+                         </div>
+                     </div>
+                 </form>
+                </div>
             </div>
         </div>
     </section>
-</body>
-</html>
+ </body>
+ </html> 
