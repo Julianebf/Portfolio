415c358d66329a55253d8e823fe12a881a68b9bf
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-12-26 11:44:44 -0300

    editando arquivos removendo biblioteca mat

--- projeto_portfolio/config/app.php
+++ projeto_portfolio/config/app.php
@@ -181,7 +181,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -210,7 +209,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+       //
     ])->toArray(),
 
 ];
